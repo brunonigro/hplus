67aade89348872ef31fd294c63627fdac4d37d71
Bruno Nigro <brunosopmac@gmail.com>
2016-09-23 19:17:06 -0300

    Adicionando tela de login

--- www/js/app.js
+++ www/js/app.js
@@ -25,13 +25,24 @@ angular.module('starter', ['ionic', 'starter.controllers'])
 .config(function($stateProvider, $urlRouterProvider) {
   $stateProvider
 
-    .state('app', {
+  .state('app', {
     url: '/app',
     abstract: true,
     templateUrl: 'templates/menu.html',
     controller: 'AppCtrl'
   })
 
+  .state('app.inicio', {
+    url: '/inicio',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/inicio.html'
+      }
+    }
+  })
+
+
+  /*
   .state('app.search', {
     url: '/search',
     views: {
@@ -42,22 +53,23 @@ angular.module('starter', ['ionic', 'starter.controllers'])
   })
 
   .state('app.browse', {
-      url: '/browse',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/browse.html'
-        }
+    url: '/browse',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/browse.html'
       }
-    })
-    .state('app.playlists', {
-      url: '/playlists',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/playlists.html',
-          controller: 'PlaylistsCtrl'
-        }
+    }
+  })
+  
+  .state('app.playlists', {
+    url: '/playlists',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/playlists.html',
+        controller: 'PlaylistsCtrl'
       }
-    })
+    }
+  })
 
   .state('app.single', {
     url: '/playlists/:playlistId',
@@ -68,6 +80,8 @@ angular.module('starter', ['ionic', 'starter.controllers'])
       }
     }
   });
+  */
+  
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
+  $urlRouterProvider.otherwise('/app/inicio');
 });
